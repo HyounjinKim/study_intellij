59d1cbf83c68c8d0bfab5ffa858f42b146703dbd
김형진 <hyoung6658@gmail.com>
2024-05-26 15:55:03 +0900

    toBinaryString 2진수를 문자열로 변경

--- ex240112/src/main/java/org/example/Ex03.java
+++ ex240112/src/main/java/org/example/Ex03.java
@@ -2,19 +2,12 @@ package org.example;
 
 import java.util.Scanner;
 
-class AA{
-    public void doA(){
-
-    }
-}
 public class Ex03 {
     public static void main(String[] args) {
-        AA aa = new AA();
-        Scanner sc = new Scanner(System.in);
+
         Integer i = new Integer(10);
         String binString = Integer.toBinaryString(i);
 
-                                        System.out.println(binString);
+        System.out.println(binString);
     }
-
 }
