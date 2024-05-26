11a2e351463d3480d13461f17927f023139b48e3
김형진 <hyoung6658@gmail.com>
2024-05-26 15:36:06 +0900

    toCharArray

--- ex240111/src/Ex02.java
+++ ex240111/src/Ex02.java
@@ -1,14 +1,8 @@
 public class Ex02 {
     public static void main(String[] args) {
-
-        System.out.println(args[0]);
-        System.out.println(args[1]);
-        System.out.println(args[2]);
-        System.out.println(args[3]);
-
         String a = "ABCDE";
 
-        char [] caa = a.toCharArray();
+        char[] caa = a.toCharArray();
 
         System.out.println(caa[0]);
 
